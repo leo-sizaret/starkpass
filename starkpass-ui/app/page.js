9623a458c5171a6074f518873ce9242d750c0efa
Sasha Abramovich <sshabramovich@Sashas-MacBook-Pro.local>
2023-07-22 18:20:18 +0200

    Added unicorns on wallet connect

--- starkpass-ui/app/page.js
+++ starkpass-ui/app/page.js
@@ -31,6 +31,88 @@ const sismoConnectConfig = {
   },
 };
 
+function drawEmojiOnCanvas() {
+  var emoji = ['🦄'];
+  // var emoji = ['💓','💕','💖','🎀','🌺','🌸']; // theme for #LetLeniLead2022
+  var totalEmojiCount = 500;
+
+  var continueDraw = false;
+  var context;
+  var canvasWidth;
+  var canvasHeight;
+  var emojies = [];
+
+  function initializeCanvas() {
+    var canvas = document.getElementsByClassName('emoji-canvas')[0];
+    context = canvas.getContext( '2d' );
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    context.scale(2, 2);
+    
+    generateCanvasSize(canvas);
+    generateEmojis();
+  }
+
+  function generateCanvasSize(canvas) {
+    var coord = canvas.getBoundingClientRect();
+    canvasWidth = coord.width;
+    canvasHeight = coord.height;
+  }
+
+  function generateEmojis() {
+    if (continueDraw === true) return;
+    emojies = [];
+    
+    for (var iterate = 0; iterate < totalEmojiCount; iterate++) {
+      var x = Math.floor(Math.random() * canvasWidth);
+      var offsetY = Math.abs(Math.floor(Math.random() * 300));
+      var fontSize = Math.floor(Math.random() * 40) + 20;
+
+      emojies.push({
+        emoji: emoji[Math.floor(Math.random() * emoji.length)],
+        x,
+        y: canvasHeight + offsetY,
+        count: Math.floor(Math.random() * 3) + 4,
+        fontSize,
+      });
+
+      if (iterate === (totalEmojiCount - 1)) {
+        continueDraw = true;
+        drawConfetti();
+        endDraw();
+      }
+    }
+  }
+
+  function drawConfetti() {
+    context.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    emojies.forEach((emoji) => {
+      drawEmoji(emoji);
+      emoji.y = emoji.y - emoji.count;
+    });
+
+    if (continueDraw) {
+      requestAnimationFrame(drawConfetti.bind(this));
+    }
+  }
+
+  function drawEmoji(emoji) {
+    context.beginPath();
+    context.font = emoji.fontSize + 'px serif';
+    context.fillText(emoji.emoji, emoji.x, emoji.y);
+  }
+
+  function endDraw() {
+    setTimeout(() => {
+      continueDraw = false;
+      context.clearRect(0, 0, canvasWidth, canvasHeight);
+    }, 5000);
+  }
+
+  initializeCanvas();
+}
+
 export default function EventsList() {
   const [hasInitialized, setHasInitialized] = useState(true);
   const [view, changeView] = useState("default");
@@ -63,17 +145,12 @@ export default function EventsList() {
   const onSignIn = useCallback(async () => {
     const starknet = await connectWallet()
     if (!starknet) {
-      toast("Rejected wallet selection or silent connect found nothing", {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "dark",
-      });
+      toast("Rejected wallet selection or silent connect found nothing");
     }
+
+    drawEmojiOnCanvas()
+
+    // toast('🦄 Auth request successful!');
 
     setAddress(starknet.account.address);
     changeView("signedIn");
@@ -85,77 +162,46 @@ export default function EventsList() {
     await buyTicket(address);
   }, [address]);
 
-  // toast('🦄 Auth request successfully approved!', {
-  //     position: "top-right",
-  //     autoClose: 5000,
-  //     hideProgressBar: false,
-  //     closeOnClick: true,
-  //     pauseOnHover: true,
-  //     draggable: true,
-  //     progress: undefined,
-  //     theme: "dark",
-  // });
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ToastContainer />
-      
-      <SismoConnectButton
-        // the client config created
-        config={sismoConnectConfig}
-        // the auth request we want to make
-        // here we want the proof of a Sismo Vault ownership from our users
-        auths={[
-          { authType: AuthType.VAULT },
-          { authType: AuthType.TELEGRAM },
-        ]}
-        // we ask the user to sign a message
-        signature={{ message: "Sign to get a ticket", isSelectableByUser: true }}
-        // onResponseBytes calls a 'setResponse' function with the responseBytes returned by the Sismo Vault
-        onResponse={(response) => {
-          onSismoConnectResponse(response);
-        }}
-        verifying={loading}
-      />
-
-      <div width="100%" height="100%" className="place-self-end">
-        {view === "default" && (
-          <p className="py-4">
-            <button className="hover:bg-white-500 text-white-700 font-semibold hover:text-black py-2 px-4 border border-white-500 rounded"
-                    onClick={onSignIn}
-                    disabled={!hasInitialized}
-            >
-              Connect a wallet
-            </button>
-          </p>
-        )}
-        {view === "signedIn" && (
-          <p className="py-4">Connected: {truncateAddress(address)}</p>
-        )}
-      </div>
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        {/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p> */}
-        {/* <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div> */}
+    <main className="flex min-h-screen flex-col items-center justify-between p-16">
+      <canvas className="emoji-canvas"></canvas>
+      <div className="z-10 w-full max-w-7xl items-center justify-between font-mono text-sm lg:flex">
+        <h2 className={`pt-5 text-2xl font-semibold place-self-start justify-self-center`}>STARKPASS</h2>
+        <div width="100%" height="100%" className="place-self-end flex items-center">
+          <div>
+            <ToastContainer />
+          </div>
+          <SismoConnectButton
+            // the client config created
+            config={sismoConnectConfig}
+            // the auth request we want to make
+            // here we want the proof of a Sismo Vault ownership from our users
+            auths={[
+              { authType: AuthType.VAULT },
+              { authType: AuthType.TELEGRAM },
+            ]}
+            // we ask the user to sign a message
+            signature={{ message: "Sign to get a ticket", isSelectableByUser: true }}
+            // onResponseBytes calls a 'setResponse' function with the responseBytes returned by the Sismo Vault
+            onResponse={(response) => {
+              onSismoConnectResponse(response);
+            }}
+            verifying={loading}
+          />
+          {view === "default" && (
+            <p className="py-4">
+              <button className="hover:bg-white-500 text-white-700 font-semibold hover:text-black py-2 px-4 border border-white-500 rounded"
+                      onClick={onSignIn}
+                      disabled={!hasInitialized}
+              >
+                Connect a wallet
+              </button>
+            </p>
+          )}
+          {view === "signedIn" && (
+            <p className="py-6">Connected: {truncateAddress(address)}</p>
+          )}
+        </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
@@ -169,7 +215,7 @@ export default function EventsList() {
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="z-10 w-full max-w-7xl grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
           <a
             href="https://www.meetup.com/starknet-london/events/294803596/"
