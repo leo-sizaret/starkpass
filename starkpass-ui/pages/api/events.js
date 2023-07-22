69aac488e6a7cd6f37bcd57812cd8516aa7b4397
Kate Zabelova <kate_zabelova@Kates-MacBook-Air.local>
2023-07-22 19:45:43 +0200

    add event get api

--- starkpass-ui/pages/api/events.js
+++ starkpass-ui/pages/api/events.js
@@ -3,15 +3,6 @@
 import { kv } from '@vercel/kv';
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    const { contractId } = req.query
-    const data = await kv.hgetall('contractId:'+ contractId);
-
-    res.status(200).json({
-        'data': data
-    })
-    res.send()
-  } 
   if (req.method === 'POST') {
     console.log(req.body)
     const reqBody = req.body
@@ -22,9 +13,15 @@ export default async function handler(req, res) {
     console.log(proofs)
 
     if (transactionId) {
-        const dataKv = 'contractId '+ contractId + ' proofs ' + JSON.stringify(proofs)
-        console.log(dataKv)
-        await kv.hset(dataKv);
+        const data = await kv.get(contractId);
+        const dataParsed = proofs
+
+        if (data) {
+          const dataParsed = data.concat(proofs)
+        }
+        console.log(dataParsed)
+
+        kv.hset(contractId, dataParsed); 
     }
 
     res.status(200)
