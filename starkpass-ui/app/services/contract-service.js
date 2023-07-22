ea551bcf8679b18b41f7d74480282aa7e0767a67
Sasha Abramovich <sshabramovich@Sashas-MacBook-Pro.local>
2023-07-22 19:47:25 +0200

    WIP getting the contracts from chain

--- starkpass-ui/app/services/contract-service.js
+++ starkpass-ui/app/services/contract-service.js
@@ -1,7 +1,6 @@
 import { getStarknet } from '@argent/get-starknet'
 import { Provider, Contract, constants } from 'starknet'
 
-import sierraProgram from '../abi/starkpass.json'
 
 export const buyTicket = async (
   accountAddress,
@@ -16,7 +15,31 @@ export const buyTicket = async (
         throw Error("starknet wallet not connected")
     }
 
-    console.log(sierraProgram)
+    const ourContract = new Contract(contractAbi, contractAddress, provider);
+
+    console.log(ourContract)
+
+    // TBD
+    // return ourContract.buyTicket(
+    //     contractAddress,
+    //     parseInputAmountToUint256(transferAmount),
+    // )
+    const balance = await ourContract.get_balance()
+    console.log('AAAAAAAAAA ======')
+    console.log(balance)
+    return balance
+}
+
+export const getEvents = async () => {
+    const provider = new Provider({ sequencer: { network: constants.NetworkName.SN_GOERLI } });
+    const contractAddress = '0x02f8dae6191b2c6584d772019a901feed6f1bcd86f3cc437781c145284c761c8'
+
+    const { abi: contractAbi } = await provider.getClassAt(contractAddress);
+    console.log(contractAbi)
+    // const starknet = getStarknet()
+    if (!starknet.isConnected) {
+        throw Error("starknet wallet not connected")
+    }
 
     const ourContract = new Contract(contractAbi, contractAddress, provider);
 
